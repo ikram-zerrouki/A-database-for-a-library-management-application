4a25b3ac09b958952c588ab090bdf3ddab734cd9
ikram-zerrouki <ki_zerrouki@esi.dz>
2024-09-22 10:58:46 +0100

    Adding Book Categories

--- seed.sql
+++ seed.sql
@@ -9,3 +9,10 @@ INSERT INTO books (title, author_id, publication_year) VALUES ('A Game of Throne
 -- Insertion des emprunts
 INSERT INTO loans (book_id, borrower_name, loan_date, return_date) VALUES (1, 'Alice Smith', '2024-01-15', '2024-02-15');
 INSERT INTO loans (book_id, borrower_name, loan_date, return_date) VALUES (2, 'Bob Johnson', '2024-02-01', NULL);
+-- Insertion des catégories
+INSERT INTO categories (name) VALUES ('Fantasy');
+INSERT INTO categories (name) VALUES ('Science Fiction');
+
+-- Mise à jour des livres avec leurs catégories
+UPDATE books SET category_id = 1 WHERE id = 1; -- Harry Potter
+UPDATE books SET category_id = 2 WHERE id = 2; -- A Game of Thrones;
